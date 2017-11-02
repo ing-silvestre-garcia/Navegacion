00502c63dc2bf70d2a2862997b04ca354215ee7b
Silvestre Garcia <silver@Silvestres-iMac.local>
2017-11-01 19:21:54 -0600

    Final

---
+++ Navegacion/TablaTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  TablaTableViewController.h
+//  Navegacion
+//
+//  Created by Silvestre Garcia on 11/1/17.
+//  Copyright © 2017 silver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TablaTableViewController : UITableViewController
+@property (weak, nonatomic) IBOutlet UIButton *btn;
+
+@end
